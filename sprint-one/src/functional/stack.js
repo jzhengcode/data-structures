6daf82f142ac1312b2d839f63ac66a6d2a54323d
Janet Zheng <janet.zhengg@gmail.com>
2020-05-13 20:27:04 -0700

    .size complete. running the program will involve an error with the test suite. to debug next

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,23 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  someInstance.store = storage;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var update = Object.keys(someInstance.store).length + 1;
+    someInstance.store[update] = value;
   };
 
   someInstance.pop = function() {
+    var mostRecent = Object.keys(someInstance.store).length;
+    var popped = someInstance.store[mostRecent];
+    delete storage[mostRecent];
+    return popped;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
