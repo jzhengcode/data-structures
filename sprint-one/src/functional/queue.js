7674dd57003de44be0c9cbca6bbc0d6dfaf10147
Janet Zheng <janet.zhengg@gmail.com>
2020-05-14 10:14:51 -0700

    optimized to consider time complexities

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,29 +4,34 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
   someInstance.store = storage;
+  var sizeStorage = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
     var entryName;
-    if (Object.keys(someInstance.store).length === 0) {
+    if (sizeStorage === 0) {
       entryName = 0;
     } else {
-      entryName = Math.max(Object.keys(someInstance.store)) + 1;
+      entryName = Math.max(...Object.keys(someInstance.store)) + 1;
     }
 
     someInstance.store[entryName] = value;
+    sizeStorage++;
   };
 
   someInstance.dequeue = function() {
     var oldestIndex = Math.min(...Object.keys(someInstance.store));
     var valListEnd = someInstance.store[oldestIndex];
     delete someInstance.store[oldestIndex];
+    if (sizeStorage > 0){
+      sizeStorage--;
+    }
     return valListEnd;
   };
 
   someInstance.size = function() {
-    return Object.keys(someInstance.store).length;
+    return sizeStorage;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,22 +4,30 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
   someInstance.store = storage;
+  var sizeStorage = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
-    var update = Object.keys(someInstance.store).length + 1;
+    // var update = Object.keys(someInstance.store).length + 1;
+    var update = sizeStorage + 1;
     someInstance.store[update] = value;
+    sizeStorage++;
   };
 
   someInstance.pop = function() {
-    var mostRecent = Object.keys(someInstance.store).length;
+    // var mostRecent = Object.keys(someInstance.store).length;
+    var mostRecent = sizeStorage;
     var popped = someInstance.store[mostRecent];
     delete storage[mostRecent];
+    if (sizeStorage > 0) {
+      sizeStorage--;
+    }
     return popped;
   };
 
   someInstance.size = function() {
-    return Object.keys(someInstance.store).length;
+    // return Object.keys(someInstance.store).length;
+    return sizeStorage;
   };
 
   return someInstance;
