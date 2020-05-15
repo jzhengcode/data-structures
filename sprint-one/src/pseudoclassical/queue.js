310cf4e72937c128796f299e915f1e72f93d8cb5
Janet Zheng <janet.zhengg@gmail.com>
2020-05-15 00:19:29 -0700

    all tests passed

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.sizeStorage = 0;
+  this.maxIndex;
+  this.minIndex;
 };
 
+Queue.prototype = {
+  enqueue: function(value) {
+    if (this.sizeStorage === 0){
+      this.storage[0] = value;
+      this.minIndex = 0;
+      this.maxIndex = 0;
+      this.sizeStorage++;
+    } else {
+      this.storage[this.maxIndex + 1] = value;
+      this.maxIndex++;
+      this.sizeStorage++;
+    }
+  },
+  dequeue: function(){
+    if (this.sizeStorage > 0) {
+      this.sizeStorage--;
+    }
 
+    var deq = this.storage[this.minIndex];
+    delete this.storage[this.minIndex];
+    this.minIndex++;
+    return deq;
+  },
+  size: function(){
+    return this.sizeStorage;
+  },
+  constructor: Queue
+};
