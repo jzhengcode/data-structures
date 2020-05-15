600176a8dcc68b46226d1170aa90701168a08ddf
Janet Zheng <janet.zhengg@gmail.com>
2020-05-14 23:11:23 -0700

    enqueue and size method tests passed

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var newInstance = Object.create(queueMethods);
+  newInstance.storage = {};
+  newInstance.sizeStorage = 0;
+  newInstance.maxIndex = 0;
+  newInstance.minIndex;
+  return newInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    if (this.sizeStorage === 0) {
+      this.storage[0] = value;
+      this.sizeStorage++;
+    } else {
+      var update = this.maxIndex + 1;
+      this.storage[update] = value;
+      this.maxIndex++;
+      this.sizeStorage++;
+    }
+  },
+  dequeue: function() {
+  },
+  size: function(){
+    return this.sizeStorage;
+  }
+};
 
 
