d578cbab31b8e1254c3e04c5ee9768e28895a397
Janet Zheng <janet.zhengg@gmail.com>
2020-05-15 00:31:58 -0700

    all tests passed

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,36 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.sizeStorage = 0;
+    this.maxIndex;
+    this.minIndex;
   }
 
+  enqueue(value) {
+    if (this.sizeStorage === 0) {
+      this.storage[0] = value;
+      this.minIndex = 0;
+      this.maxIndex = 0;
+      this.sizeStorage++;
+    } else {
+      this.storage[this.maxIndex + 1] = value;
+      this.maxIndex++;
+      this.sizeStorage++;
+    }
+  }
+
+  dequeue() {
+    if (this.sizeStorage > 0) {
+      this.sizeStorage--;
+    }
+
+    var deq = this.storage[this.minIndex];
+    delete this.storage[this.minIndex];
+    this.minIndex++;
+    return deq;
+  }
+
+  size() {
+    return this.sizeStorage;
+  }
 }
