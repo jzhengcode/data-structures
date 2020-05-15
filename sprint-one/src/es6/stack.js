568fd0bbfffdffb7520a13f9077c8cb04d797ef4
Janet Zheng <janet.zhengg@gmail.com>
2020-05-15 00:27:35 -0700

    all tests passed

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,34 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.sizeStorage = 0;
+    this.maxIndex;
   }
 
+  push(value){
+    if (this.sizeStorage === 0) {
+      this.storage[0] = value;
+      this.maxIndex = 0;
+      this.sizeStorage++;
+    } else {
+      this.storage[this.maxIndex + 1] = value;
+      this.maxIndex++;
+      this.sizeStorage++;
+    }
+  }
+
+  pop(){
+    if (this.sizeStorage > 0) {
+      this.sizeStorage--;
+    }
+
+    var pop = this.storage[this.maxIndex];
+    delete this.storage[this.maxIndex];
+    this.maxIndex--;
+    return pop;
+  }
+
+  size(){
+    return this.sizeStorage;
+  }
 }
