d10691b06af2a9e4937b517b5c5a112b0b3ed736
Janet Zheng <janet.zhengg@gmail.com>
2020-05-14 23:49:33 -0700

    size method test passed

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,17 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.sizeStorage = 0;
+  this.maxIndex;
 };
 
 
+Stack.prototype = {
+  push: function(value){},
+  pop: function(){},
+  size: function(){
+    return this.sizeStorage;
+  },
+  constructor: Stack
+}
