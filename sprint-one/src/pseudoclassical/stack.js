334efe177d3a489243b1d307c41c0ea81b43eaed
Janet Zheng <janet.zhengg@gmail.com>
2020-05-15 00:06:26 -0700

    all tests passed

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -8,10 +8,29 @@ var Stack = function() {
 
 
 Stack.prototype = {
-  push: function(value){},
-  pop: function(){},
+  push: function(value){
+    if (this.sizeStorage === 0) {
+      this.storage[0] = value;
+      this.maxIndex = 0;
+      this.sizeStorage++;
+    } else {
+      this.storage[this.maxIndex + 1] = value;
+      this.maxIndex++;
+      this.sizeStorage++;
+    }
+  },
+  pop: function(){
+    if (this.sizeStorage > 0){
+      this.sizeStorage--;
+    }
+
+    var pop = this.storage[this.maxIndex];
+    delete this.storage[this.maxIndex];
+    this.maxIndex--;
+    return pop;
+  },
   size: function(){
     return this.sizeStorage;
   },
   constructor: Stack
-}
+};
