ec5b45272deb80452b53038169b6fe0cde0f9971
Janet Zheng <janet.zhengg@gmail.com>
2020-05-16 14:16:35 -0700

    linkedlists and tree tests passed

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,58 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  extend(newTree, treeMethods);
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
+  
+};
+
+var extend = function(to, from){
+  for(var key in from){
+    to[key]=from[key];
+  }
+
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  /*should invoke a new tree
+  new tree should be placed in newtree.children */
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  // var body = this.Tree;
+  // var look=[]
+  // var search=function(body){
+  //   if(body){
+  //     look.push(body)
+  //   }
+  //   if(body.children){
+  //     for (var i =0; i<body.children.lengthistory; i++){
+  //       search(body.children[i])
+  //     }
+  //   }
+  // }
+  if (this.value === target) {
+    return true;
+  } else {
+    if (this.children.length > 0) {
+
+      for (var i = 0; i < this.children.length; i++) {
+        // if ^ returns true, exit loop
+        if (this.children[i].contains(target) === true) {
+          return true;
+        }
+        // otherwise, continue looping
+      }
+    }
+  }
+
+  return false;
 };
 
 
