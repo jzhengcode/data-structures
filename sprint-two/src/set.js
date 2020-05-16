abf65c06936ee5af1ba410dea23dfb8fc2e56247
Janet Zheng <janet.zhengg@gmail.com>
2020-05-16 16:01:21 -0700

    pseudo code deleted

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,14 +7,6 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  // I - some string
-  // O - nothing. side effect, storage in set should be updated
-  // C - constraints. has to be a string
-  // E - if the string already exists into the storage, then don't add it
-
-  //if the item isn't a property in the storage
-    // store the item as a property
-  // otherwise, return
   if (this._storage[item] === undefined) {
     this._storage[item] = item;
   } 
@@ -23,15 +15,6 @@ setPrototype.add = function(item) {
 };
 
 setPrototype.contains = function(item) {
-  // I - some string
-  // O - boolean
-  // C - item must be a string. output must be a boolen
-  // E - none
-
-  // if item is a property in the storage
-    // return true
-  //otherwise
-    // return false
   if (this._storage[item] === item) {
     return true;
   } else {
@@ -41,13 +24,6 @@ setPrototype.contains = function(item) {
 
 setPrototype.remove = function(item) {
   delete this._storage[item];
-  /*
-  i-string
-  o-returns nothing, #side effect its deletes item
-  c-must be a string
-  e-none, but if string doesnt exist then nothing should naturaly happen
-  */
- //it search the object for item and delete that key value pair 
 };
 
 /*
