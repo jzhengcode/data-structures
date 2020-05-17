b7c86850a1829623f0d0b015641c36959f20c028
Janet Zheng <janet.zhengg@gmail.com>
2020-05-17 00:55:40 -0700

    debugger removed

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -24,9 +24,6 @@ describe('binarySearchTree', function() {
     binarySearchTree.insert(2);
     binarySearchTree.insert(3);
     binarySearchTree.insert(7);
-    debugger;
-    binarySearchTree.contains(7);
-    binarySearchTree.contains(8)
     expect(binarySearchTree.contains(7)).to.equal(true);
     expect(binarySearchTree.contains(8)).to.equal(false);
   });
